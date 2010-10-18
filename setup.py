7bcf35ea11359c17877263402acf825e05c4c76c
James Dennis <james@extension.fm>
2010-10-18 00:25:05 -0400

    Adding setup file

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+ 
+from distutils.core import setup
+ 
+setup(name='RocketNest',
+      version='0.1',
+      description='Python Client Library for the EchoNest API',
+      author='James Dennis',
+      author_email='james@extension.fm',
+      url='http://github.com/ExtensionFM/RocketNest',
+      py_modules=['rocketnest'])
